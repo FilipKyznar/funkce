8b55a3d6782afc623a10f6ab0de86f80d1499dc0
Filip.Kyznar <kyznarf@gmail.com>
2024-03-01 12:31:50 +0100

    funkce

---
+++ objem koule.py
@@ -0,0 +1,11 @@
+def objemkoule(polomer):
+    if polomer !=0:
+        vysledek = 4/3 * 3,14 * (polomer**3)
+        return vysledek
+    else :
+        print("Nelze dělit nulou")
+
+polomer = int(input("Zadejte promenou"))
+
+print("vysledek je: ",)
+
